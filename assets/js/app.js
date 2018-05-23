366f0aaf3b320470ed5ab221c7f99d698287404a
Unknown <axelfertinel@gmail.com>
2018-05-23 10:53:51 +0200

    Auto stash before rebase of "dev"
    WebPack fonctionnel

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+console.log('Bienvenue sur WebPack!')
+ import $ from "jquery"
+
+  $(document).ready(function () {
+      console.log("ok, JQuery fonctionne !");
+  });
+
+  console.log('Bienvenue sur WebPack!')
+
