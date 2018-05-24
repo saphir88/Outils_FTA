daebddf3f5407c04d833c69219625fd2be9681e9
Cyril Bellamy <cyrilbellamy1@gmail.com>
2018-05-24 09:02:54 +0200

    ajout fomulaire inscription

--- src/AppBundle/Form/CommunautesType.php
+++ src/AppBundle/Form/CommunautesType.php
@@ -3,6 +3,11 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +18,8 @@ class CommunautesType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nomStartup')->add('description')->add('video')->add('siteWeb')->add('categorie')->add('siret')->add('adresse')->add('nomContact')->add('mail')->add('telephone');
+        $builder->add('nomStartup')->add('description', TextareaType::class, array(
+            'required'   => false))->add('video')->add('siteWeb')->add('categorie')->add('siret')->add('adresse')->add('nomContact')->add('mail', EmailType::class)->add('telephone');
     }/**
      * {@inheritdoc}
      */
