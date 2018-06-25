cc8542f7d4c4e03075da857090234b6679f6e386
Cyril Bellamy <cyrilbellamy1@gmail.com>
2018-06-25 08:58:20 +0200

    .

--- src/AppBundle/Form/CommunauteType.php
+++ src/AppBundle/Form/CommunauteType.php
@@ -28,8 +28,7 @@ class CommunauteType extends AbstractType
         $builder
             ->add('nomStartup', TextType::class, array('label'=>'Nom de la startup* :'))
             ->add('nomSociete', TextType::class, array('label'=>'Nom de la societe :'))
-            ->add('file', FileType::class, ['label' => 'Logo :', 'data_class' => null])
-
+            ->add('file', FileType::class, ['label' => 'Logo* :', 'data_class' => null])
             ->add('description', TextareaType::class, array('required'=> false, 'label'=>'Description :'))
             ->add('siteWeb', TextType::class, array('required'=> true, 'label'=>'Site Web* :'))
             ->add('video', TextType::class, array('required' => false , 'label' => 'Lien Youtube :'))
