e1d4ed67b23144dcc16c8285566c0cdd57c3e452
Thomas Catella <spectralys88@gmail.com>
2018-07-03 10:16:34 +0200

    differenciation entre visiteurs et investisseurs et modification des vues et de l'extraction CSV en conséquences

--- src/AppBundle/Form/ParticipantType.php
+++ src/AppBundle/Form/ParticipantType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,10 +16,15 @@ class ParticipantType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('prenom', TextType::class)
-            ->add('nom', TextType::class)
-            ->add('mail', EmailType::class)
-            ->add('societe', TextType::class, ['required' => false]);
+        $builder->add('prenom', TextType::class, ['label' => 'Prénom* :'])
+            ->add('nom', TextType::class, ['label' => 'Nom* :'])
+            ->add('mail', EmailType::class, ['label' => 'Mail* :'])
+            ->add('statut', ChoiceType::class, array('label'=>'Statut* :',
+                'choices'  => array(
+                    'Visiteur' => 'Visiteur',
+                    'Investisseur' => 'Investisseur',
+                ),'placeholder' => 'Choisissez votre statut'))
+            ->add('societe', TextType::class, ['required' => false, 'label' => 'Société :']);
 
     }/**
      * {@inheritdoc}
