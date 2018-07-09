3483496c8d62aa3d1790c6d4d52206268a2a1cb7
Cyril Bellamy <cyrilbellamy1@gmail.com>
2018-07-09 21:34:22 +0200

    mise en place de l'harmonisation des style sur les outils

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 var Encore = require('@symfony/webpack-encore');
 
-    Encore
-        .setOutputPath('web/build/')  
-        .setPublicPath('/web')  
-        .addEntry('app', './assets/js/app.js')  
-        .addEntry('style', './assets/scss/main.scss')
-        .addEntry('logo', './assets/css/style.css')
-        .cleanupOutputBeforeBuild()
-        .enableBuildNotifications()
-        .enableSassLoader();
+Encore
+    .setOutputPath('web/build/')
+    .setPublicPath('/web')
+    .addEntry('app', './assets/js/app.js')
+    .addEntry('main', './assets/scss/main.scss')
+    .addEntry('event', './assets/css/style.css')
+    .addEntry('inscription', './assets/scss/inscription.scss')
+    .addEntry('compte', './assets/scss/compte.scss')
+    .addEntry('validation', './assets/scss/validation.scss')
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSassLoader();
 
-    module.exports = Encore.getWebpackConfig();
+module.exports = Encore.getWebpackConfig();
